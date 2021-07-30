62f0b31f826206b791f2627ce5e168afdd9736a2
BoWei <i19017496@student.newinti.edu.my>
2021-07-30 18:24:56 +0800

    30/07/2021 1824pm

--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserHome.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserHome.java
@@ -2,7 +2,9 @@ package com.example.water_quality_monitring_and_reporting_app_with_ai_chatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserHome extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class UserHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+    }
+
+    public void toReportMenu(View view) {
+        Intent intent = new Intent(this, UserReportMenu.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserReportMenu.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserReportMenu.java
@@ -10,5 +10,6 @@ public class UserReportMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_report_menu);
+
     }
 }
