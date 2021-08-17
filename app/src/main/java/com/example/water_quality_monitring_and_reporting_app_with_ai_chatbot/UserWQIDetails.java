f1f76cdcb4225d5c39c416be6e34ded9cd8eda97
BoWei <i19017496@student.newinti.edu.my>
2021-08-17 22:25:29 +0800

    17/08/2021 2225pm

--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserWQIDetails.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserWQIDetails.java
@@ -6,11 +6,14 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class UserWQIDetails extends AppCompatActivity {
     private TextView userWQIDetails_txt_title;
+
+    private ImageView userWQIDetails_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,8 @@ public class UserWQIDetails extends AppCompatActivity {
 
         userWQIDetails_txt_title = findViewById(R.id.userWQIDetails_txt_title);
 
+        userWQIDetails_img = findViewById(R.id.userWQIDetails_img);
+
         setTitleAndContent(indicator);
     }
 
@@ -44,35 +49,45 @@ public class UserWQIDetails extends AppCompatActivity {
 
     public void setTitleAndContent(String indicator){
         String title = "";
+        int drawable = R.drawable.rivericon;
         switch(indicator){
             case "WQI":
                 title = "Water Quality Index (WQI)";
+
                 break;
 
             case "DO":
                 title = "Dissolved Oxygen (DO)";
+                drawable = R.drawable.dissolvedoxygenicon;
                 break;
 
             case "BOD":
                 title = "Biochemical Oxygen Demand (BOD)";
+                drawable = R.drawable.bodicon;
                 break;
 
             case "COD":
                 title = "Chemical Oxygen Demand (COD)";
+                drawable = R.drawable.codicon;
                 break;
 
             case "NH3N":
                 title = "Ammoniacal Nitrogen (NH3-N)";
+                drawable = R.drawable.nh3nicon;
                 break;
 
             case "SS":
                 title = "Suspended Solids (SS)";
+                drawable = R.drawable.ssicon;
                 break;
 
             case "pH":
                 title = "pH Value";
+                drawable = R.drawable.phicon;
                 break;
         }
+
+        userWQIDetails_img.setImageDrawable(getResources().getDrawable(drawable));
         userWQIDetails_txt_title.setText(title);
     }
 
