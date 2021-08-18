3cd1bf0eeb9fff2bef204d19776e20371cafe0b3
BoWei <i19017496@student.newinti.edu.my>
2021-08-18 13:58:34 +0800

    18/08/2021 1358pm

--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/Login.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/Login.java
@@ -3,6 +3,7 @@ package com.example.water_quality_monitring_and_reporting_app_with_ai_chatbot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+
     }
 }
