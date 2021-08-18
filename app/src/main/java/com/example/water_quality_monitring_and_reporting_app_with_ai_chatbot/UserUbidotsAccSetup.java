936d794276202f0629a4a6408a99b6599a30e152
BoWei <i19017496@student.newinti.edu.my>
2021-08-18 23:07:04 +0800

    18/08/2021 2306pm

---
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserUbidotsAccSetup.java
@@ -0,0 +1,14 @@
+package com.example.water_quality_monitring_and_reporting_app_with_ai_chatbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserUbidotsAccSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_ubidots_acc_setup);
+    }
+}
