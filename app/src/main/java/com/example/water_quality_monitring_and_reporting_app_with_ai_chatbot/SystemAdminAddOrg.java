55c5e80fc58c98a687a4dda8b37f26fbf5b1aacb
BoWei <i19017496@student.newinti.edu.my>
2021-08-30 15:55:58 +0800

    30/08/2021 1555pm

--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/SystemAdminAddOrg.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/SystemAdminAddOrg.java
@@ -4,9 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SystemAdminAddOrg extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+public class SystemAdminAddOrg extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private TextInputEditText systemAdminAddOrg_txtInputET_orgName, systemAdminAddOrg_txtInputET_addressLine,
+            systemAdminAddOrg_txtInputET_postcode, systemAdminAddOrg_txtInputET_city;
+
+    private Spinner systemAdminAddOrg_spinner_state;
+
+    private TextView systemAdminAddOrg_txt_errorName, systemAdminAddOrg_txt_errorAddress;
+
+    private Boolean orgNameValid = false, orgAddressValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +39,62 @@ public class SystemAdminAddOrg extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        systemAdminAddOrg_txtInputET_orgName = findViewById(R.id.systemAdminAddOrg_txtInputET_orgName);
+        systemAdminAddOrg_txtInputET_addressLine = findViewById(R.id.systemAdminAddOrg_txtInputET_addressLine);
+        systemAdminAddOrg_txtInputET_postcode = findViewById(R.id.systemAdminAddOrg_txtInputET_postcode);
+        systemAdminAddOrg_txtInputET_city = findViewById(R.id.systemAdminAddOrg_txtInputET_city);
+        systemAdminAddOrg_spinner_state = findViewById(R.id.systemAdminAddOrg_spinner_state);
+
+        systemAdminAddOrg_txt_errorName = findViewById(R.id.systemAdminAddOrg_txt_errorName);
+        systemAdminAddOrg_txt_errorAddress = findViewById(R.id.systemAdminAddOrg_txt_errorAddress);
+
+        systemAdminAddOrg_txtInputET_orgName.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(systemAdminAddOrg_txtInputET_orgName.getText().toString().isEmpty()){
+                    systemAdminAddOrg_txt_errorName.setVisibility(View.VISIBLE);
+                    orgNameValid = false;
+                }else{
+                    systemAdminAddOrg_txt_errorName.setVisibility(View.GONE);
+                    orgNameValid = true;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        addressValidation(systemAdminAddOrg_txtInputET_addressLine);
+        addressValidation(systemAdminAddOrg_txtInputET_postcode);
+        addressValidation(systemAdminAddOrg_txtInputET_city);
+
+        if (systemAdminAddOrg_spinner_state != null) {
+            systemAdminAddOrg_spinner_state.setOnItemSelectedListener(this);
+
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.state_array, android.R.layout.simple_spinner_item);
+
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource
+                    (android.R.layout.simple_spinner_dropdown_item);
+            // Apply the adapter to the spinner.
+            if (systemAdminAddOrg_spinner_state != null) {
+                systemAdminAddOrg_spinner_state.setAdapter(adapter);
+            }
+        }
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String spinnerLabel = adapterView.getItemAtPosition(position).toString();
+        //displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
 
     @Override //when back button clicked
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -27,4 +103,64 @@ public class SystemAdminAddOrg extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void addressValidation(TextInputEditText txtInputET_addressAttribute){
+        txtInputET_addressAttribute.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try{
+                    String addressLineCheck = "", postcodeCheck = "", cityCheck = "";
+
+                    addressLineCheck = systemAdminAddOrg_txtInputET_addressLine.getText().toString();
+                    postcodeCheck = systemAdminAddOrg_txtInputET_postcode.getText().toString();
+                    cityCheck = systemAdminAddOrg_txtInputET_city.getText().toString();
+
+                    if(addressLineCheck.isEmpty() || postcodeCheck.isEmpty() || cityCheck.isEmpty()){
+                        String errorMsgAddress = "Require: "; int errorAmount = 0;
+
+                        if(addressLineCheck.isEmpty()){
+                            errorMsgAddress += "address line";
+                            errorAmount++;
+                        }
+
+                        if(postcodeCheck.isEmpty()){
+                            errorMsgAddress += (errorAmount > 0) ? ", postcode" : "postcode";
+                            errorAmount++;
+                        }
+
+                        if(cityCheck.isEmpty()){
+                            errorMsgAddress += (errorAmount > 0) ? ", city" : "city";
+                            errorAmount++;
+                        }
+
+                        systemAdminAddOrg_txt_errorAddress.setText(errorMsgAddress);
+                        orgAddressValid = false;
+                    }
+                    else{
+                        systemAdminAddOrg_txt_errorAddress.setText("");
+                        orgAddressValid = true;
+                    }
+                }catch(Exception e){
+                    System.out.println(e.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+    }
+
+    public void add(View view) {
+        if(orgNameValid && orgAddressValid){
+            displayToast("Success!");
+        }else{
+            displayToast("Please make sure every crendetial is filled in correctly");
+        }
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
 }
