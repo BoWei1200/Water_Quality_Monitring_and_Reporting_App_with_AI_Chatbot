b4ea6cb112c45be376e0c04c65f84888226e8d83
BoWei <i19017496@student.newinti.edu.my>
2021-08-31 16:34:30 +0800

    31/08/2021 1634pm

--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/DatabaseHelper.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/DatabaseHelper.java
@@ -360,20 +360,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public String getAPIKey(String userID){
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT ubidotsAPI FROM " + TABLE_USER_UBIDOTS_CREDENTIALS + " WHERE ubidotsUserID=? ", new String[]{userID});;
+        Cursor cursor = db.rawQuery("SELECT ubidotsAPI FROM " + TABLE_USER_UBIDOTS_CREDENTIALS + " WHERE ubidotsUserID=? ", new String[]{userID});
 
         return (cursor.moveToFirst()) ? cursor.getString(cursor.getColumnIndex("ubidotsAPI")) : "";
     }
 
     public String getorgID(String orgName){
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT orgID FROM " + TABLE_ORGANIZATION + " WHERE orgName=? ", new String[]{orgName});;
+        Cursor cursor = db.rawQuery("SELECT orgID FROM " + TABLE_ORGANIZATION + " WHERE orgName=? ", new String[]{orgName});
 
         return (cursor.moveToFirst()) ? cursor.getString(cursor.getColumnIndex("orgID")) : "";
     }
 
+    public Cursor getOrgInfoByUserID(String userID){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT org.orgName FROM " + TABLE_ORGANIZATION + " org, "+ TABLE_EMPLOYEE_ORGANIZATION +" employOrg WHERE employOrg.userID=? AND org.orgID = employOrg.orgID", new String[]{userID});
+    }
 
-
+    public Cursor getInvestigatorTeamInfoByUserID(String userID){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT inTeam.investigationTeamID, inTeam.investigationTeamName FROM " + TABLE_INVESTIGATION_TEAM + " inTeam, "+ TABLE_INVESTIGATION_TEAM_MEMBER +" inTeamMem WHERE inTeamMem.investigationTeamUserID=? AND inTeamMem.investigationTeamID = inTeam.investigationTeamID", new String[]{userID});
+    }
 
     // Registered user info
     public Cursor readInfo(String userEmail) {
--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/InvestigatorHome.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/InvestigatorHome.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +16,9 @@ public class InvestigatorHome extends AppCompatActivity {
 
     private SharedPreferences mPreferences;
     private String sharedPrefFile = "com.example.android.fyp_hydroMyapp";
+    private final String userIDPreference = "userID";
+
+    private String getUserIDPreference = "";
 
     private ImageView investigatorHome_img_setting, investigatorHome_img_refresh;
     private PopupMenu investigatorHome_popupMenu_setting;
@@ -27,6 +31,7 @@ public class InvestigatorHome extends AppCompatActivity {
         setContentView(R.layout.activity_investigator_home);
 
         mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        getUserIDPreference = mPreferences.getString(userIDPreference, null);
 
         investigatorHome_img_setting =findViewById(R.id.investigatorHome_img_setting);
         investigatorHome_img_refresh = findViewById(R.id.investigatorHome_img_refresh);
@@ -57,6 +62,27 @@ public class InvestigatorHome extends AppCompatActivity {
 
         DatabaseHelper dbHelper = new DatabaseHelper(this);
 
+        Cursor cursorGetOrgInfo = dbHelper.getOrgInfoByUserID(getUserIDPreference);
+        Cursor cursorGetInvestigatorTeamInfo = dbHelper.getInvestigatorTeamInfoByUserID(getUserIDPreference);
+
+        try {
+            if(cursorGetOrgInfo.moveToFirst() && cursorGetInvestigatorTeamInfo.moveToFirst()){
+                String orgName = cursorGetOrgInfo.getString(cursorGetOrgInfo.getColumnIndex("orgName"));
+
+                String teamID = cursorGetInvestigatorTeamInfo.getString(cursorGetInvestigatorTeamInfo.getColumnIndex("investigationTeamID"));
+                String teamName = cursorGetInvestigatorTeamInfo.getString(cursorGetInvestigatorTeamInfo.getColumnIndex("investigationTeamName"));
+
+                investigatorHome_txt_orgName.setText(orgName);
+
+                investigatorHome_txt_investigationTeamID.setText("Team ID: " + teamID);
+                investigatorHome_txt_investigationTeamName.setText(teamName);
+            }
+        }catch (Exception e){
+            System.out.println("ERROR: "+ e.toString());
+        }
+
+
+
     }
 
     public void settings(View view) {
@@ -69,5 +95,26 @@ public class InvestigatorHome extends AppCompatActivity {
     }
 
     public void toOtherPages(View view) {
+        Intent intent = new Intent();
+
+        switch(view.getId()){
+            case R.id.investigatorHome_cv_myInvestigationTeam:
+                intent = new Intent(this, InvestigatorTeamDetailPage.class);
+                break;
+
+            case R.id.investigatorHome_cv_pendingInvestigation:
+                intent = new Intent(this, InvestigatorTeamDetailPage.class);
+                break;
+
+            case R.id.investigatorHome_cv_myInvestigation:
+                intent = new Intent(this, InvestigatorTeamDetailPage.class);
+                break;
+
+            case R.id.investigatorHome_cv_news:
+                intent = new Intent(this, NewsList.class);
+                break;
+        }
+
+        startActivity(intent);
     }
 }
