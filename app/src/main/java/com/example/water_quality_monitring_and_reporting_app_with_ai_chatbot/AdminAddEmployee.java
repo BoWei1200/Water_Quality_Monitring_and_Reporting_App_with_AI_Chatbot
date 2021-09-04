f930cac59e52ebb5e513f865dc9a86fcdb1944b3
BoWei <i19017496@student.newinti.edu.my>
2021-09-04 17:30:39 +0800

    04/09/2021 1730pm

--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/AdminAddEmployee.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/AdminAddEmployee.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -18,6 +20,11 @@ public class AdminAddEmployee extends AppCompatActivity {
             adminAddEmployee_txtInputET_city;
 
     private Spinner adminAddEmployee_spinner_state;
+
+    private RadioGroup adminAddEmployee_rGroup_employeeType;
+
+    private TextView adminAddEmployee_txt_errorName, adminAddEmployee_txt_errorEmail, adminAddEmployee_txt_errorPhone,
+            adminAddEmployee_txt_errorAddress, adminAddEmployee_txt_errorUserType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +46,14 @@ public class AdminAddEmployee extends AppCompatActivity {
         adminAddEmployee_txtInputET_city = findViewById(R.id.adminAddEmployee_txtInputET_city);
 
         adminAddEmployee_spinner_state = findViewById(R.id.adminAddEmployee_spinner_state);
-        
 
+        adminAddEmployee_rGroup_employeeType = findViewById(R.id.adminAddEmployee_rGroup_employeeType);
+
+        adminAddEmployee_txt_errorName = findViewById(R.id.adminAddEmployee_txt_errorName);
+        adminAddEmployee_txt_errorEmail = findViewById(R.id.adminAddEmployee_txt_errorEmail);
+        adminAddEmployee_txt_errorPhone = findViewById(R.id.adminAddEmployee_txt_errorPhone);
+        adminAddEmployee_txt_errorAddress = findViewById(R.id.adminAddEmployee_txt_errorAddress);
+        adminAddEmployee_txt_errorUserType = findViewById(R.id.adminAddEmployee_txt_errorUserType);
 
 
     }
@@ -55,5 +68,29 @@ public class AdminAddEmployee extends AppCompatActivity {
 
     public void addEmployee(View view) {
 
+    }
+
+    public void selectUserType(View view) {
+        switch (adminAddEmployee_rGroup_employeeType.getCheckedRadioButtonId()){
+            case R.id.adminAddEmployee_rBtn_AD:
+                displayToast("AD");
+                break;
+
+            case R.id.adminAddEmployee_rBtn_EX:
+                displayToast("EX");
+                break;
+
+            case R.id.adminAddEmployee_rBtn_IN:
+                displayToast("IN");
+                break;
+
+            case R.id.adminAddEmployee_rBtn_RH:
+                displayToast("RH");
+                break;
+        }
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(AdminAddEmployee.this,message,Toast.LENGTH_SHORT).show();
     }
 }
