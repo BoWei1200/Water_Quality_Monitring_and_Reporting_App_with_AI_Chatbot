67018698d8a350ae7439a826f933bc25c91633c0
BoWei <i19017496@student.newinti.edu.my>
2021-09-06 02:19:00 +0800

    06/09/2021 0218am

--- app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserReportStatus.java
+++ app/src/main/java/com/example/water_quality_monitring_and_reporting_app_with_ai_chatbot/UserReportStatus.java
@@ -3,6 +3,7 @@ package com.example.water_quality_monitring_and_reporting_app_with_ai_chatbot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class UserReportStatus extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class UserReportStatus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_report_status);
+    }
+
+    public void viewPhoto(View view) {
     }
 }
